ab0b3b66b41de40779777a0b69a51c60f330c42e
ajaiswal <anshul.chander@gmail.com>
2020-04-01 15:59:49 +1100

    Configured listing and relators apps

--- btre/settings.py
+++ btre/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
+    'realtors.apps.RealtorsConfig',
+    
 ]
 
 MIDDLEWARE = [
--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'listings/listings.html')
+
+def listings(request):
+    return render(request, 'listings/listing.html')
+
+def seacrh(request):
+    return render(request, 'listings/search.html')
